5a4d8ea50a5eb6bfe707c9b7ab675a7bc7534271
Rob Pungello <rob.pungello@gmail.com>
2022-07-19 12:43:36 -0400

    Add application ID to config

--- config/pdf-shipping.php
+++ config/pdf-shipping.php
@@ -4,4 +4,5 @@
 return [
     'url' => env('PDF_SHIPPING_URL', 'https://shipping.pdfsystems.com'),
     'token' => env('PDF_SHIPPING_TOKEN'),
+    'application_id' => env('PDF_SHIPPING_APPLICATION_ID', 0),
 ];

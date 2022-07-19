5c828ad6b60fa61ea2aa7a0199dafaeb5b53b6a4
Rob Pungello <rob.pungello@gmail.com>
2022-07-19 12:11:45 -0400

    Rename test

---
+++ tests/ShippingTest.php
@@ -0,0 +1,6 @@
+<?php
+
+it('can create shipping client', function () {
+    $client = new Pdfsystems\PdfShipping\PdfShipping();
+    expect($client)->toBeInstanceOf(\Pdfsystems\PdfShipping\PdfShipping::class);
+});

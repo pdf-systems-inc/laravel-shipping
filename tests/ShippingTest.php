218968d9f44b78192cd295de1aa14a77adf29a37
Rob Pungello <rob.pungello@gmail.com>
2022-08-22 09:09:54 -0400

    Remove debugging from test

--- tests/ShippingTest.php
+++ tests/ShippingTest.php
@@ -3,7 +3,5 @@
 it('can create shipping client', function () {
     $client = new Pdfsystems\PdfShipping\PdfShipping();
 
-    ray($client->getShipments('woven'));
-
     expect($client)->toBeInstanceOf(\Pdfsystems\PdfShipping\PdfShipping::class);
 });
